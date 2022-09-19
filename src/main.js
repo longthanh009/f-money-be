8c796040f6bca7e5471bb7aef1e7293ef9463315
khuatlong0902 <khuatlong0902@gmail.com>
2022-09-19 10:03:36 +0700

    Create project f-money

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// Router
+
+
+// connect database MongoBb
+mongoose.connect("mongodb://localhost:27017/f-money-clc")
+.then(() =>{
+    console.log("Kết nối db thành công");
+})
+app.listen(8000, () => {
+    console.log(`Server is running on port: 8000`);
+})
