f2fd50b7cde3e023c67d6de0294dfd9bab98df70
Phan Vuong <pdvuongtb@gmail.com>
2022-10-13 22:24:40 +0700

    api-change-password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -69,3 +69,37 @@ export const userLogin = async (req, res) => {
   
 	  res.json({ status: 'error', error: 'Invalid username/password' })
   }
+
+  // Đổi mật khẩu
+export const changePassword = async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+  
+	  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		  return res.json({ status: 'error', error: 'Invalid password' })
+	  }
+  
+	if (plainTextPassword.length < 5) {
+		  return res.json({
+			  status: 'error',
+			  error: 'Password too small. Should be atleast 6 characters'
+		  })
+	  }
+	try {
+		  const user = jwt.verify(token, JWT_SECRET)
+  
+		  const _id = user.id
+  
+		  const password = await bcrypt.hash(plainTextPassword, 10)
+  
+		  await User.updateOne(
+			  { _id },
+			  {
+				  $set: { password }
+			  }
+		  )
+		  res.json({ status: 'ok' })
+	  } catch (error) {
+		  console.log(error)
+		  res.json({ status: 'error', error: ';))' })
+	  }
+}
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { userLogin, userRegistration } from "../controllers/auth";
+import { userLogin, userRegistration,changePassword } from "../controllers/auth";
 const router = express.Router();
 
 router.post("/register", userRegistration);
 router.post("/login", userLogin);
-router.post("/change-password", );
+router.post("/change-password", changePassword);
 
 export default router;
