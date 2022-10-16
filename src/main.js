1a04206b207a91a1bb7a4690b21b880050679459
le dung <ledung.14111998@gmail.com>
2022-10-16 10:44:25 +0700

    API CRUD Service

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import routeAuth from "./routes/auth.js"
+import routerService from "./routes/service";
 const app = express();
 
 app.use(cors());
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
 
 // Router
 app.use("/api/auth", routeAuth);
-
+app.use("/api", routerService);
 
 app.listen(PORT, () => {
     console.log(`APi is Running on http://localhost:${PORT}`);
