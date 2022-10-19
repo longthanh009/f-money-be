dfc38dce9c9ce2d61e478278afe1923f994066b5
Phan Vuong <pdvuongtb@gmail.com>
2022-10-19 21:28:06 +0700

    FM-75-api-change-password-lender

--- src/controllers/userLender.js
+++ src/controllers/userLender.js
@@ -68,3 +68,37 @@ res.json({ status: 'ok' })
   
 	  res.json({ status: 'error', error: 'Tên người dùng hoặc mật khẩu không hợp lệ!' })
   }
+
+  // Đổi mật khẩu
+export const userLenderChangePassword = async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+  
+	  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		  return res.json({ status: 'error', error: 'Mật khẩu không hợp lệ!' })
+	  }
+  
+	if (plainTextPassword.length < 5) {
+		  return res.json({
+			  status: 'error',
+			  error: 'Mật khẩu quá ngắn. Mật khẩu phải trên 6 ký tự!'
+		  })
+	  }
+	try {
+		  const user = jwt.verify(token, JWT_SECRET)
+  
+		  const _id = user.id
+  
+		  const password = await bcrypt.hash(plainTextPassword, 10)
+  
+		  await userLender.updateOne(
+			  { _id },
+			  {
+				  $set: { password }
+			  }
+		  )
+		  res.json({ status: 'ok' })
+	  } catch (error) {
+		  console.log(error)
+		  res.json({ status: 'error', error: ';))' })
+	  }
+}
--- src/routes/userLender.js
+++ src/routes/userLender.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { userLenderLogin, userLenderRegistration } from "../controllers/userLender";
+import { userLenderChangePassword, userLenderLogin, userLenderRegistration } from "../controllers/userLender";
 const router = express.Router();
 
 router.post("/userLender/register", userLenderRegistration);
 router.post("/userLender/login", userLenderLogin);
+router.post("/userLender/change-password", userLenderChangePassword);
 
 export default router;
