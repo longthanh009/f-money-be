e80ac545615f6c98b4615b8ee542ba549cb18437
Phan Vuong <pdvuongtb@gmail.com>
2022-10-19 23:33:23 +0700

    FM-82-api-delete-customer

--- src/routes/userCustomer.js
+++ src/routes/userCustomer.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { updateUserCustomer, userCustomerChangePassword, userCustomerLogin, userCustomerRegistration } from "../controllers/userCustomer";
+import { deleteUserCustomer, updateUserCustomer, userCustomerChangePassword, userCustomerLogin, userCustomerRegistration } from "../controllers/userCustomer";
 const router = express.Router();
 
 router.post("/userCustomer/register", userCustomerRegistration);
 router.post("/userCustomer/login", userCustomerLogin);
 router.post("/userCustomer/change-password", userCustomerChangePassword);
 router.put("/userCustomer/:id", updateUserCustomer);
-// router.delete("/userCustomer/:id", deleteUserLender);
+router.delete("/userCustomer/:id", deleteUserCustomer);
 // router.get("/userCustomer/:id", getUserLender);
 // router.get("/userCustomer", getUsersLenders);
 // router.get("/userCustomer/logout", logoutUsersLenders);
