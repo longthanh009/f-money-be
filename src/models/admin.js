b7524d46b84748bcdc3f02ebfc174a6d5f82a030
Phan Vuong <pdvuongtb@gmail.com>
2022-10-19 20:39:28 +0700

    API - Signup - Admin

---
+++ src/models/admin.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    Role: { type: String,
+        default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
