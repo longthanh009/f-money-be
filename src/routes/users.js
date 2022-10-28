366b5b426ea25bb77ede0de81bd7695a2eaf88fa
Phan Vuong <pdvuongtb@gmail.com>
2022-10-29 01:08:50 +0700

    FM-97-api-history-account

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { Login, logout, refreshToken, Registration } from "../controllers/auth";
+import { history, Login, logout, refreshToken, Registration } from "../controllers/auth";
 
 const router = express.Router();
 
 router.post("/users/register", Registration);
 router.post("/users/login", Login);
 router.get("/users/logout", logout);
-router.get('/users/refresh_token', refreshToken)
+router.get('/users/refresh_token', refreshToken);
+router.get('/history', history);
 router.post("/users/change-password", );
 router.put("/users/:id", );
 router.delete("/users/:id", );
