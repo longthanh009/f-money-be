db38e6cb3df46cbc79262da40c9526530a2eefdc
Phan Vuong <pdvuongtb@gmail.com>
2022-10-29 22:33:30 +0700

    FM-98-api-change-password-account

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, logout, Registration } from "../controllers/auth";
+import { login, logout, Registration, usersChangePassword } from "../controllers/auth";
 import { deleteUser, getUser, getUsers, searchUsers, updateUser } from "../controllers/users";
 
 const router = express.Router();
@@ -25,7 +25,7 @@ const router = express.Router();
 router.post("/users/register", Registration);
 router.post("/users/login", login);
 router.get("/users/logout", logout);
-// router.post("/users/change-password", usersChangePassword);
+router.post("/users/change-password", usersChangePassword);
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 router.get("/users/:id", getUser);
