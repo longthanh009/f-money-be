6eb14bcc71a97c063441b64c8785e25420a2726c
Duy Ha <havanduy@gmail.com>
2022-10-30 13:22:16 +0700

    FM-33 creact new -api more contract

--- src/controllers/contract.js
+++ src/controllers/contract.js
@@ -1,9 +1,19 @@
 import Contract from "../models/contract";
+
 export const getContracts = async(req,res) => {
     try {
         const data = await Contract.find({})
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json("Lỗi Contract!")
+    }
+}
+
+export const createContracts = async(req,res) => {
+    try {
+        const contracts = await Contract(req.body).save();
+        res.status(200).json(contracts);
+    } catch (error) {
+        res.status(500).json("Thêm Thành công")
     }
 }
--- src/routes/contract.js
+++ src/routes/contract.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {getContracts} from "../controllers/contract";
+import {getContracts,createContracts} from "../controllers/contract";
 const router = express.Router();
 
 router.put("/contract/:id", );
 router.delete("/contract/:id", );
 router.get("/contract/:id", );
 router.get("/contracts", getContracts);
-router.post("/contracts", )
+router.post("/contracts", createContracts)
 
 export default router;
