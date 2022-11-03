be511af972bb756d2b81a685ba07e25e26ad65d5
Phan Vuong <pdvuongtb@gmail.com>
2022-11-03 22:25:09 +0700

    login error

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import connectDB from "./config/db.js";
 import cors from "cors";
 import dotenv from "dotenv";
 import routerContract from './routes/contract.js'
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import routerUsers from "./routes/users"
 
 
@@ -21,6 +21,24 @@ app.use(bodyParser.json())
 
 dotenv.config();
 
+//swagger
+app.use(express.json());
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Customer API",
+            description: "Customer API Information",
+            contact: {
+                name: "Amazing Developer"
+            },
+            servers: ["http://localhost:5000"]
+        }
+    },
+    apis: ['.routes/*.js']
+};
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
+
 
 const PORT = process.env.PORT || 9000;
 connectDB();
@@ -29,8 +47,9 @@ app.get("/", (req, res) => {
     res.send("Backend is Running..");
 });
 
-app.use("/api" , routerContract)
+app.use("/api", routerContract)
 app.use("/api", routerUsers);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 // app.use("/api/users", routeUsers);
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,20 +7,31 @@ const router = express.Router();
 /**
  * @swagger
  * tags:
- *   name: Users
- *   description: The users managing API
+ *  name: User
+ *  description: This is for the main data
  */
 
 /**
  * @swagger
  * /api/users:
- *   get:
- *     summary: Returns the list of all the users
- *     tags: [Users]
- *     responses:
- *       200:
- *         description: The list of the users
+ *  GET:
+ *    description: users to request all customers
+ *    responses:
+ *      '200':
+ *        description: A successful response
  */
+router.get("/users", getUsers);
+
+/**
+ * @swagger
+ * /api/users/:id:
+ *  GET:
+ *    description: users to request all customers
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+router.get("/users/:id", getUser);
 
 router.post("/users/register", Registration);
 router.post("/users/login", login);
@@ -28,8 +39,8 @@ router.get("/users/logout", logout);
 router.post("/users/change-password", usersChangePassword);
 router.put("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
-router.get("/users/:id", getUser);
-router.get("/users", getUsers);
+
+
 router.get("/users/search/:key", searchUsers);
 
 
