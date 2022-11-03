3575b63f7f436fc29794f78db423836978d6cc17
Duy Ha <havanduy@gmail.com>
2022-11-03 15:03:47 +0700

    FM-86 creact router contractDetail

---
+++ src/routes/contractDetail.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+
+router.put("/contractDetail/:id", );
+router.delete("/contractDetail/:id", );
+router.get("/contractDetail/:id", );
+router.get("/contractDetails", );
+router.post("/contractDetails", )
+
+export default router;
