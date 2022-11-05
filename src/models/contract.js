12e0e15abd747b810c08cf98fe8c3771a270ddec
Duy Ha <havanduy@gmail.com>
2022-11-05 22:17:02 +0700

    FM-33 Update model

--- src/models/contract.js
+++ src/models/contract.js
@@ -1,75 +1,126 @@
 import mongoose from "mongoose";
-import users from "./users"
-import  contractDetail  from "./contractDetail"
+import users from "./users";
+import contractDetail from "./contractDetail"
 const Contract = new mongoose.Schema(
-    {
-        //loại hợp đồng
-        TypeContract: {
-            type : String,
-        },
-        // name người vay
-        customerName: {
-            type : String,
-            require: true,
-        },
-        userLeaderId:{
-            type: Object,
-            ref: users
-        },
-        userCustomerId:{
-            type: Object,
-            ref: users
-        },
-        //tổng tiền vay(bát họ)
-        totalLoanAmount:{
-            type: Number,
-        },
-        // Tiền đưa khách
-        customersGiveMoney:{
-            type: Number,
-        },
-        // số ngày đóng
-        daysPayable:{
-            type: String,
-        },
-        // ngày vay(ngày bắt đầu và ngày kết thúc)
-        borrowingDate:{
-            type: String,
-        },
-        // tỉ lệ    
-        ratio:{
-            type: String,
-        },
-        // Tiền đóng 1 ngày
-        onePayPayment:{
-            type: Number,
-        },
-        // số tiền đã đóng
-        amountPaid:{
-            type: Number,
-        },
-        //ngày con lại phải đóng
-        remainingAmount:{
-            type: Number,
-        },
-        // ngày phải đóng   
-        closingDate:{
-            type: String,
-        },
-        contractDetailsId:{
-           type: Object,
-           ref:contractDetail
-        },
-        statusCustomer:{
-            type: Number,
-            default: true,
-        },
-        statusContract:{
-            type: Number,
-            default: true,
-        }
+  {
+    // loại họp đồng
+    typeContract: {
+      type: Number,
+      default: false,
+      require: true,
     },
-    { timestamps: true }
+    // tên khách hàng
+    customerName: {
+      type: String,
+      require: true,
+    },
+    // mã khách hàng
+    contractCode: {
+      type: String,
+      require: true,
+    },
+    // thẻ căn cước công dân
+    cmnd: {
+      type: Number,
+      require: true,
+    },
+    // số điện thoại
+    phoneNumber: {
+      type: Number,
+      require: true,
+    },
+    // địa chỉ
+    address: {
+      type: String,
+      require: true,
+    },
+    // Tiền khách hàng nhận được
+    moneyReceivedByCustomer: {
+      type: Number,
+      require: true,
+    },
+    // tiền phải trả
+    moneyToBePaid: {
+      type: Number,
+      require: true,
+    },
+    //Bốc trong vòng
+    borrowMoneyForHowManyDays: {
+      type: Number,
+      require: true,
+    },
+    // số ngày thanh toán 1 lần
+    numberOfDaysOf1Payment: {
+      type: Number,
+      require: true,
+    },
+    // ngày vay tiền
+    loanDate: {
+      type: String,
+      require: true,
+    },
+    // ghi chú
+    desc: {
+      type: String,
+    },
+    //ID Ng cho vay    ----->
+    userLeaderId: {
+      type: Object,
+      ref: users,
+    },
+    //ID khách hàng
+    userCustomerId: {
+      type: Object,
+      ref: users,
+    },
+    // trạng thái của người vay
+    statusCustomer: {
+      type: Number,
+      default: false,
+    },
+    // trạng thái hợp đồng
+    statusContract: {
+      type: Object,
+    },
+    //tiền đóng 1 ngày
+    oneDayPayment: {
+      type: Number,
+    },
+    // ngày phải đóng tiền (hôm nay, ngày mai , 20/12/20021)
+    paymentDate: {
+      type: String,
+    },
+    // Tổng số tiền khách đã đóng
+    amountPaidCustomer: {
+      type: Number,
+    },
+    // số tiền còn lại phải trả
+    remainingAmountPaid: {
+      type: Number,
+    },
+    // nợ cũ
+    oldDebt: {
+      type: Number,
+    },
+    // ngày kết thúc vay tiền
+    loanClosingDate: {
+      type: String,
+    },
+    // Tổng lãi phí
+    totalInterest: {
+      type: Number,
+    },
+    ///------>                                    chi tiết hợp đồng
+    // ngày họ (từng ngày theo từng kỳ)
+    // tiền họ (tiền đóng theo từng ngày)
+    // ngày giao dịch các khoản đóng tiền (ngày nào đã đóng)
+    // tiền khách trả ( theo từng ngày)
+    contractDetailsId:{
+      type: Object,
+      ref:contractDetail
+   },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Contract", Contract);
