b31fd951cf57fc1d3907187afe560e7bad2599f5
le dung <ledung.14111998@gmail.com>
2022-11-14 14:02:42 +0700

    fix update and delete dv

--- src/controllers/servicePack.js
+++ src/controllers/servicePack.js
@@ -3,37 +3,46 @@ import servicePack from "../models/servicePack"
 // list 
 export const listService = async(req,res) => {
     try {
-        const data = await servicePack.find({})
-        res.json(data);
+        const data =  await servicePack.find().exec();
+        res.json(data)
     } catch (error) {
-        res.json(error)
+        res.json({
+            error : "Không thêm được"
+        })
     }
 }
-// thêm 
+// creat
 export const createService = async(req,res) => {
     try {
-        const data = await servicePack(req.body).save();
-        res.json(data);
+        const data = await new servicePack(req.body).save()
+        res.json(data)
     } catch (error) {
-        res.json(error)
+        res.json({
+            error : "Không thêm được"
+        })
     }
 }
-// xóa 
+// delete
 export const deleteService = async(req,res) => {
     try {
-        const data = await servicePack({_id : req.params.id}).exec()
+        const data = await servicePack.deleteOne({_id : req.params.id}).exec()
         res.json(data);
     } catch (error) {
-        res.json(error)
+        res.status("400").json({
+            error : "Lỗi không xoá được"
+        })
     }
 }
 
 // update 
 export const updateService = async(req,res) => {
     try {
-        const data = await servicePack({_id : req.params.id},req.body).exec()
+        const data = await servicePack.findOneAndUpdate({_id:req.params.id},req.body,{new:true}).exec()
         res.json(data);
     } catch (error) {
-        res.json(error)
+        res.status("400").json({
+            error : "Lỗi không update được"
+        })
     }
+    
 }
