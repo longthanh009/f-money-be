a01b8531bcaf0d78a1a3f9581c78b8f399982cfe
khuatlong0902 <khuatlong0902@gmail.com>
2023-01-06 09:55:05 +0700

    Update-login-refresToken

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import moment from 'moment';
 const JWT = '8hEnPGeoBqGUT6zksxt4G95gW+uMdzwe7EVaRnp0xRI=';
 import { createError } from "../utils/error.js";
 
@@ -7,7 +8,6 @@ export const verifyToken = (req, res, next) => {
     if (!token) {
         return next(createError(401, "You are not authenticated!"));
     }
-
     jwt.verify(token, JWT, (err, user) => {
         if (err) return next(createError(403, "Token is not valid!"));
         req.user = user;
@@ -43,3 +43,32 @@ export const createToken = (id) => {
         expiresIn: maxAge
     });
 };
+export const jwtVerifyToken = async (req, res, next) => {
+    const token = req.headers['authorization'] ? req.headers['authorization'].split(" ") : null
+    if (!token) {
+        return res.status(401).json({
+            message: 'Vui lòng đăng nhập.'
+        })
+    }
+    else {
+        try {
+            jwt.verify(token[0], 'sontv', { algorithm: 'HS256' }, (err, decoded) => {
+                if (err) {
+                    return res.status(401).json(err)
+                }
+                req.user = decoded;
+                if (moment().format('X') > req.user.exp) {
+                    return res.status(401).json({
+                        message: "Vui lòng đăng nhập lại"
+                    })
+                }
+
+                next()
+            })
+        } catch (error) {
+            return res.status(400).json({
+                message: error.message
+            })
+        }
+    }
+}
