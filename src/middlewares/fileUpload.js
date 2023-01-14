a19045c5dbeb1259ccd23e8be47566b9c5367f62
Phan Vuong <pdvuongtb@gmail.com>
2023-01-15 04:09:46 +0700

    Multiple Image Uploads

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,15 +1,53 @@
-import multer from "multer";
-const path = require('path')
-const sourcePath = require('app-root-path').path
+/** @format */
+
+import multer from 'multer';
+
+//Storage Setting
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(sourcePath, 'src/uploads'))
-    },
-    filename: function (req, file, cb) {
-        const filename = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, filename + '-' + file.originalname)
+  destination: './public/images', //directory (folder) setting
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname); // file name setting
+  },
+});
+
+//Upload Setting
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 25 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == 'image/jpeg' ||
+      file.mimetype == 'image/jpg' ||
+      file.mimetype == 'image/png' ||
+      file.mimetype == 'image/gif'
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      cb(new Error('Only jpeg,  jpg , png, and gif Image allow'));
     }
-})
-export const upload = () => multer({
-    storage: storage,
-})
+  },
+});
+
+// app.post('/image', upload.array('products', 12), (req, res) => {
+//   const result = imageModel.create({
+//     name: req.body.name,
+//     img: req.files,
+//   });
+//   if (result) {
+//     res.send({ code: 200, message: 'Upload Success' });
+//   } else {
+//     res.send({ code: 500, message: 'Upload Err' });
+//   }
+// });
+
+// app.get('/image', async (req, res) => {
+//   let products = await imageModel.find({});
+//   if (products.length > 0) {
+//     res.send({ code: 200, data: products });
+//   } else {
+//     res.send({ code: 500, message: 'Server Err' });
+//   }
+// });
+
+export default upload;
